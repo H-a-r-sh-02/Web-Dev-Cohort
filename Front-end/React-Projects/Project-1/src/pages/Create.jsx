1d824bd9e5a222e24c0e7f2e6ed44d6820309c76
Harsh Pandey <harshp3465@gmail.com>
2025-07-07 23:59:01 +0530

    Just write the code for create a recipe and show it on recipe section...!

--- Front-end/React-Projects/Project-1/src/pages/Create.jsx
+++ Front-end/React-Projects/Project-1/src/pages/Create.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useForm } from "react-hook-form"
 import { nanoid } from "nanoid"
+import { recipeContext } from '../context/RecipeContext';
 
 const Create = () => {
-  const { register, handleSubmit } = useForm();
+  const {data, setdata} = useContext(recipeContext);
+  const { register, handleSubmit, reset } = useForm();
 
-  const submitHandler = (data)=>{
-    console.log(data);
-    data.id = nanoid();
+  const submitHandler = (recipe)=>{
+    recipe.id = nanoid();
+    setdata([...data, recipe]);
+    reset();
   }
 
   return (
     <form onSubmit={handleSubmit(submitHandler)} className='w-[45%] h-[75%] px-10 py-2 mt-8 mx-66 rounded bg-slate-800'>
       <h2 className='mb-4 text-2xl font-thin text-center'>Create Recipe</h2>
+
       <input className='block border-b outline-0 p-2' type='url' {...register("image")} placeholder='Enter Image URL' />
       <small className='text-red-400'>This is how error is shown</small>
 
@@ -24,7 +28,7 @@ const Create = () => {
 
       <textarea className='block border-b outline-0 p-2' {...register("instruction")} placeholder='//Write instructions in points'></textarea>
       
-      <select className='mt-3'>
+      <select className='block border-b outline-0 p-2 bg-slate-800' {...register("category")}>
         <option value="cat-1">Category-1</option>
         <option value="cat-2">Category-2</option>
         <option value="cat-3">Category-3</option>
--- Front-end/React-Projects/Project-1/src/pages/Recipe.jsx
+++ Front-end/React-Projects/Project-1/src/pages/Recipe.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { recipeContext } from '../context/RecipeContext'
 
 const Recipe = () => {
+  const {data} = useContext(recipeContext);
+
+  const renderRecipe = data.map(recipe=><div key={recipe.id}><h1>{recipe.title}</h1></div>);
   return (
-    <div></div>
+    <div>
+      {renderRecipe}
+      </div>
   )
 }
 
