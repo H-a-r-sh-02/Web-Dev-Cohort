3c2c8acfb69a4187467b0aff4693a44f68b335bf
Harsh Pandey <harshp3465@gmail.com>
2025-07-23 02:12:19 +0530

    port changes!

--- Back-end/Day-7/Backend/server.js
+++ Back-end/Day-7/Backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const app = require('./src/app');
 const connectToDb = require('./src/db/db');
+const port = process.env.PORT || 3000;
 
 connectToDb();
 
@@ -9,6 +10,6 @@ app.get('/', (req, res)=>{
 
 });
 
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000!");
+app.listen(port, ()=>{
+    console.log(`Server is running on ${port}!`);
 }); 
