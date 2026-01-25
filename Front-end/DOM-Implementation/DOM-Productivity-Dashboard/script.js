adcf4a74aa6b905349c1192e5f078ac5b1b7c935
Harsh Pandey <harshp3465@gmail.com>
2026-01-26 00:36:27 +0530

    completed the Motivation-Page section!

--- Front-end/DOM-Implementation/DOM-Productivity-Dashboard/script.js
+++ Front-end/DOM-Implementation/DOM-Productivity-Dashboard/script.js
@@ -25,12 +25,11 @@ function todo() {
   if (localStorage.getItem("currentTask")) {
     currentTask = JSON.parse(localStorage.getItem("currentTask"));
   } else {
-    console.log("Task list is empty");
   }
 
   function renderTask() {
-    var allTask = document.querySelector(".taskList");
-    var sum = "";
+    let allTask = document.querySelector(".taskList");
+    let sum = "";
     currentTask.forEach(function (elem, idx) {
       sum += `<div class="list">
                         <h5>${elem.task}
@@ -67,5 +66,53 @@ function todo() {
     location.reload();
   });
 }
+function dailyPlanner () {
+var dayContainer = document.querySelector(".dailyContainer");
+var dayData = JSON.parse(localStorage.getItem('dayData')) || {};
+var hours = Array.from({length:18}, (elem, idx) => `${6+idx}:00 - ${7+idx}:00`);
+var wholeDaySum ='';
+
+
+
+hours.forEach((elem, idx) => {
+  var saveData = dayData[idx] || "";
+  wholeDaySum += `<div class="day">
+                    <p>${elem}</p>
+                    <input id=${idx} type="text" placeholder="..." value=${saveData}>
+                </div>`
+
+              });
+dayContainer.innerHTML = wholeDaySum;
+
+var dayInputs = document.querySelectorAll('.dailyContainer .day input');
+
+dayInputs.forEach(function (elem) {
+  elem.addEventListener('input', function () {
+    dayData[elem.id] = elem.value;
+    localStorage.setItem('dayData', JSON.stringify(dayData))
+  });  
+});
+}
+function motivation () {
+var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var date = new Date();
+document.querySelector(".dayBox .day").innerText = days[date.getDay()];
+document.querySelector(".dayBox .date").innerText = date.getDate(); 
+async function quotes() {
+  try {
+    const res = await fetch('https://quotes-api-self.vercel.app/quote');
+    const data = await res.json();
+    
+    document.querySelector('.lower p').innerText = data.quote;
+    document.querySelector('.lower h2').innerText = `${data.author}~`;
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+quotes();
+}
 openCards();
 todo();
+dailyPlanner();
+motivation ();
