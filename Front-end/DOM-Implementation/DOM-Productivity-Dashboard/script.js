104ec00aa930ed4faed47407c11a830e4d00133e
Harsh Pandey <harshp3465@gmail.com>
2026-01-28 00:27:02 +0530

    Completed the Pomodoro section!

--- Front-end/DOM-Implementation/DOM-Productivity-Dashboard/script.js
+++ Front-end/DOM-Implementation/DOM-Productivity-Dashboard/script.js
@@ -66,53 +66,115 @@ function todo() {
     location.reload();
   });
 }
-function dailyPlanner () {
-var dayContainer = document.querySelector(".dailyContainer");
-var dayData = JSON.parse(localStorage.getItem('dayData')) || {};
-var hours = Array.from({length:18}, (elem, idx) => `${6+idx}:00 - ${7+idx}:00`);
-var wholeDaySum ='';
+function dailyPlanner() {
+  var dayContainer = document.querySelector(".dailyContainer");
+  var dayData = JSON.parse(localStorage.getItem("dayData")) || {};
+  var hours = Array.from(
+    { length: 18 },
+    (elem, idx) => `${6 + idx}:00 - ${7 + idx}:00`,
+  );
+  var wholeDaySum = "";
 
-
-
-hours.forEach((elem, idx) => {
-  var saveData = dayData[idx] || "";
-  wholeDaySum += `<div class="day">
+  hours.forEach((elem, idx) => {
+    var saveData = dayData[idx] || "";
+    wholeDaySum += `<div class="day">
                     <p>${elem}</p>
                     <input id=${idx} type="text" placeholder="..." value=${saveData}>
-                </div>`
+                </div>`;
+  });
+  dayContainer.innerHTML = wholeDaySum;
 
-              });
-dayContainer.innerHTML = wholeDaySum;
+  var dayInputs = document.querySelectorAll(".dailyContainer .day input");
 
-var dayInputs = document.querySelectorAll('.dailyContainer .day input');
-
-dayInputs.forEach(function (elem) {
-  elem.addEventListener('input', function () {
-    dayData[elem.id] = elem.value;
-    localStorage.setItem('dayData', JSON.stringify(dayData))
-  });  
-});
+  dayInputs.forEach(function (elem) {
+    elem.addEventListener("input", function () {
+      dayData[elem.id] = elem.value;
+      localStorage.setItem("dayData", JSON.stringify(dayData));
+    });
+  });
 }
-function motivation () {
-var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-var date = new Date();
-document.querySelector(".dayBox .day").innerText = days[date.getDay()];
-document.querySelector(".dayBox .date").innerText = date.getDate(); 
-async function quotes() {
-  try {
-    const res = await fetch('https://quotes-api-self.vercel.app/quote');
-    const data = await res.json();
-    
-    document.querySelector('.lower p').innerText = data.quote;
-    document.querySelector('.lower h2').innerText = `${data.author}~`;
+function motivation() {
+  var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  var date = new Date();
+  document.querySelector(".dayBox .day").innerText = days[date.getDay()];
+  document.querySelector(".dayBox .date").innerText = date.getDate();
+  async function quotes() {
+    try {
+      const res = await fetch("https://quotes-api-self.vercel.app/quote");
+      const data = await res.json();
 
-  } catch (err) {
-    console.error(err);
+      document.querySelector(".lower p").innerText = data.quote;
+      document.querySelector(".lower h2").innerText = `${data.author}~`;
+    } catch (err) {
+      console.error(err);
+    }
   }
+  quotes();
 }
-quotes();
+function pomodoro() {
+  var start = document.querySelector(".pomo-card .controls .start-timer");
+  var pause = document.querySelector(".pomo-card .controls .pause-timer");
+  var reset = document.querySelector(".pomo-card .controls .reset-timer");
+  var session = document.querySelector(".pomo-timer .session");
+
+  var isWorkSession = true;
+  let timerInterval = null;
+
+  let totalSecond = 25 * 60;
+  let timer = document.querySelector(".pomo-timer .pomo-card h1");
+
+  function upDateTimer() {
+    let minutes = Math.floor(totalSecond / 60);
+    let seconds = totalSecond % 60;
+
+    timer.innerHTML = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+  }
+  function startTimer() {
+    clearInterval(timerInterval);
+    if (isWorkSession) {
+      timerInterval = setInterval(() => {
+        if (totalSecond > 0) {
+          totalSecond--;
+          upDateTimer();
+        } else {
+          isWorkSession = !isWorkSession;
+          clearInterval(timerInterval);
+          timer.innerHTML = "05:00";
+          session.innerText = "Break Session";
+          totalSecond = 5 * 60;
+        }
+      }, 1000);
+    } else {
+      timerInterval = setInterval(() => {
+        if (totalSecond > 0) {
+          totalSecond--;
+          upDateTimer();
+        } else {
+          isWorkSession = !isWorkSession;
+          clearInterval(timerInterval);
+          timer.innerHTML = "25:00";
+          session.innerText = "Work Session";
+          totalSecond = 25 * 60;
+        }
+      }, 1000);
+    }
+  }
+  function pauseTimer() {
+    clearInterval(timerInterval);
+  }
+  function resetTimer() {
+    clearInterval(timerInterval);
+    totalSecond = 25 * 60;
+    upDateTimer();
+  }
+
+  start.addEventListener("click", startTimer);
+  pause.addEventListener("click", pauseTimer);
+  reset.addEventListener("click", resetTimer);
 }
+
 openCards();
 todo();
 dailyPlanner();
-motivation ();
+motivation();
+pomodoro();
