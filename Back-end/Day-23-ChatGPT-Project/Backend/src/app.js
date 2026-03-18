84b4f658a128ca87a483e5c3b5193099d670bc74
Harsh Pandey <harshp3465@gmail.com>
2026-03-19 01:06:12 +0530

    added frontend build files !

--- Back-end/Day-23-ChatGPT-Project/Backend/src/app.js
+++ Back-end/Day-23-ChatGPT-Project/Backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const path = require('path');
+
 /* ROUTES */
 const authRoutes = require('./routes/auth.routes');
 const chatRoutes = require('./routes/chat.routes');
@@ -16,9 +18,14 @@ app.use(cors(
 /* Middlewares */
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
 
 /* Using Routes */
 app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
+
+app.get("*name", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 module.exports = app;
