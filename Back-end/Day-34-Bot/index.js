5b603c8fef119ec5be9bdceb8dae6007cfaa63bb
Harsh Pandey <harshp3465@gmail.com>
2026-03-24 01:52:49 +0530

    Completed Discord bot integration!

--- Back-end/Day-34-Bot/index.js
+++ Back-end/Day-34-Bot/index.js
@@ -11,36 +11,53 @@ const client = new Client({
 });
 
 const ai = new GoogleGenAI({
-    apiKey: process.env.GEMINI_API_KEY
+  apiKey: process.env.GEMINI_API_KEY,
 });
 
-async function generateImage(prompt) {
+/* Image generation code VALID ONLY WITH PAID API KEY */
+// async function generateImage(prompt) {
+//   const response = await ai.models.generateContent({
+//     model: "gemini-2.5-flash-image",
+//     contents: prompt,
+//   });
+//   for (const part of response.candidates[0].content.parts) {
+//     if (part.text) {
+//       console.log(part.text);
+//     } else if (part.inlineData) {
+//       const imageData = part.inlineData.data;
+//       const buffer = Buffer.from(imageData, "base64");
+//       return buffer;
+//     }
+//   }
+// }
+
+/* Text generation code */
+async function generateContent(prompt) {
   const response = await ai.models.generateContent({
-    model: "gemini-3.1-flash-image-preview",
+    model: "gemini-3-flash-preview",
     contents: prompt,
   });
-  for (const part of response.candidates[0].content.parts) {
-    if (part.text) {
-      console.log(part.text);
-    } else if (part.inlineData) {
-      const imageData = part.inlineData.data;
-      const buffer = Buffer.from(imageData, "base64");
-      return buffer;
-    }
-  }
+
+  return response.text;
 }
 
-  client.once("ready", () => {
-    console.log("Bot is ready!");
-  });
-  client.on("messageCreate", async (message) => {
-    const isBot = message.author.bot;
-    if (isBot) return;
+client.once("ready", () => {
+  console.log("Bot is ready!");
+});
+client.on("messageCreate", async (message) => {
+  const isBot = message.author.bot;
+  if (isBot) return;
+  /* Image generation code */
+  // const imageBuffer = await generateImage(message.content);
+  // if (imageBuffer) {
+  //     const attachment = new AttachmentBuilder(imageBuffer, { name: 'generated-image.png' });
+  //     message.channel.send({ files: [attachment] });
+  // }
 
-    const imageBuffer = await generateImage(message.content);
-    if (imageBuffer) {
-        const attachment = new AttachmentBuilder(imageBuffer, { name: 'generated-image.png' });
-        message.channel.send({ files: [attachment] });
-    }
-  });
+  /* Text generate */
+  const content = await generateContent(message.content);
+  if (content) {
+    message.reply(content);
+  }
+});
 client.login(process.env.DISCORD_BOT_TOKEN);
