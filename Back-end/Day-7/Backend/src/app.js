68e97e5dc118829932f66f19aa72d1dd3dac8229
Harsh Pandey <harshp3465@gmail.com>
2026-03-29 00:31:35 +0530

    changes to the Day-7 files!

--- Back-end/Day-7/Backend/src/app.js
+++ Back-end/Day-7/Backend/src/app.js
@@ -1,23 +1,12 @@
 const express = require("express");
 const songRoutes = require("./routes/song.routes");
 const cors = require("cors");
-const path = require("path");
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
-
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(cors());
 
 app.use("/", songRoutes);
 
-app.get("*name", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
 
 module.exports = app;
