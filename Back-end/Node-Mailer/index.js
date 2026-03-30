be4fce37b2064609cf2d2ebc8e2f16f7ec32fc70
Harsh Pandey <harshp3465@gmail.com>
2026-03-30 19:01:25 +0530

    Completed google-auth & node-mailer modules!

---
+++ Back-end/Node-Mailer/index.js
@@ -0,0 +1,4 @@
+const sendEmail = require('./email');
+
+
+sendEmail("harshpandey200416@gmail.com", "Testing_Mail", "This is a test mail sending with the purpose to check the node mailer is working or not!", "<b>This is a test mail </b>");
